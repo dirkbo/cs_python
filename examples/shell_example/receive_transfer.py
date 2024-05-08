8e64c4ca1447a806127a15c4f8b980b209034185
Dirk B <dirkbo@googlemail.com>
2024-05-08 18:07:49 +0200

    simplified receive file example

--- examples/shell_example/receive_transfer.py
+++ examples/shell_example/receive_transfer.py
@@ -4,7 +4,36 @@ from tqdm import tqdm
 from cryptshare.Client import Client as CryptshareClient
 
 
+def download_file(dl_server, file, directory):
+    response = requests.get(dl_server + file["href"], stream=True)
+    full_path = os.path.join(directory, file["fileName"])
+    os.makedirs(directory, exist_ok=True)
+    with open(full_path, "wb") as handle:
+        for data in tqdm(
+            response.iter_content(),
+            desc=file["fileName"],
+            unit="B",
+            unit_scale=True,
+            unit_divisor=1024,
+            total=int(file["size"]),
+        ):
+            handle.write(data)
+
+
 def receive_transfer(origin, dl_server, recipient_transfer_id, password, save_path):
+    """
+    Downloads a transfer from a Cryptshare server.
+
+    Parameters:
+    origin (str): The origin of the request.
+    dl_server (str): The URL of the Cryptshare server.
+    recipient_transfer_id (str): The Transfer ID of the transfer to download.
+    password (str): The password for the transfer.
+    save_path (str): The path where the downloaded files should be saved.
+
+    Returns:
+    None
+    """
     cryptshare_client = CryptshareClient(dl_server)
     #  Reads existing verifications from the 'store' file if any
     cryptshare_client.read_client_store()
@@ -13,29 +42,13 @@ def receive_transfer(origin, dl_server, recipient_transfer_id, password, save_pa
     #  Both branches also react on the REST API not licensed
     if cryptshare_client.exists_client_id() is False:
         cryptshare_client.request_client_id()
-    else:
-        # Check CORS state for a specific origin.
-        cryptshare_client.cors(origin)
 
+    directory = os.path.join("transfers", save_path)
+    print(f"Downloading Transfer {recipient_transfer_id} from {dl_server}...")
     download = cryptshare_client.download(recipient_transfer_id, password)
     files_info = download.download_files_info()
-    directory = os.path.join("transfers", save_path)
-    print(f"Downloading Transfer {recipient_transfer_id} from {dl_server} to {directory}...")
+
     for file in files_info:
-        response = requests.get(dl_server + file["href"], stream=True)
-        full_path = os.path.join(directory, file["fileName"])
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        print(f"Downloading {file['fileName']} to {full_path}")
-        with open(full_path, "wb") as handle:
-            for data in tqdm(
-                response.iter_content(),
-                desc=file["fileName"],
-                unit="B",
-                unit_scale=True,
-                unit_divisor=1024,
-                total=int(file["size"]),
-            ):
-                handle.write(data)
+        download_file(dl_server, file, directory)
 
     print(f"Downloaded Transfer {recipient_transfer_id} to {directory} complete.")
