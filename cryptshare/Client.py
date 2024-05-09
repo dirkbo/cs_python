c245126e5d33da54cec91cd3c6afa9e2194afa35
Dirk B <dirkbo@googlemail.com>
2024-05-09 11:59:15 +0200

    Actions updated, dev_requirements including flak8-bugbear

--- cryptshare/Client.py
+++ cryptshare/Client.py
@@ -1,5 +1,6 @@
 import json
 import logging
+
 import requests
 
 import cryptshare.Download as Download
--- examples/shell_example/helpers.py
+++ examples/shell_example/helpers.py
@@ -1,6 +1,5 @@
-import os
 import logging
-
+import os
 from datetime import datetime, timedelta
 
 logger = logging.getLogger(__name__)
